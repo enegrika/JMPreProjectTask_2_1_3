0eaa4e823cf2d4d8566eee296a9ee71164865878
Sergey Groshev <sergeigroshev@gmail.com>
2021-05-05 18:13:31 +0700

    add dog.class

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,6 +1,13 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
+
+    @Autowired
+    private AnimalsCage animalsCage;
 
     private Long nanoTime = System.nanoTime();
 
